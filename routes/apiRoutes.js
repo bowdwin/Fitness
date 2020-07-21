10f85442e5b01ceb86551b2bfa906c9574c72d13
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-07-21 16:14:24 -0400

    getting error on server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,6 @@
-//
-// const router = require('express').Router();
-// const Workout = require('../Develop/models/Workout');
+const router = require('express').Router();
+const Workout = require('../models/workout');
 
-// router.post('/api/workout', (req, res) => {
-//   Workout.Create({}).then();
-// });
-//main point deploying to heroku with mongo
-//https://unh.bootcampcontent.com/unh-coding-bootcamp/unh-por-fsf-pt-03-2020-u-c/blob/master/17-NoSQL/04-Supplemental/MongoDBHerokuDeploy.md
+router.post('/api/workout', (req, res) => {
+  Workout.Create({}).then();
+});
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const logger = require('morgan');
+// const logger = require('morgan');
 const mongoose = require('mongoose');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3000;
-
+// const db = require('./models');
 const app = express();
-
 // middleware
-app.use(logger('dev'));
+// app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 
 let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
 
@@ -30,3 +29,4 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+module.exports = router;
