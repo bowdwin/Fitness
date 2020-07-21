91411a8ab743a5354ac7aa9046f009c95e306020
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-07-21 14:05:02 -0400

    added schema

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
+const logger = require('morgan');
 const mongoose = require('mongoose');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// middleware
+app.use(logger('dev'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./Develop/public'));
 
-// routes
-app.use(require('./routes/api.js'));
-//app.use(routes);
+app.use(express.static('public'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout_trackerDB',
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-);
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// set up routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
