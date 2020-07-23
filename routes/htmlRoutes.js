4d6407feb5c4215879b0126735a66c988d5112ed
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-07-23 12:11:58 -0400

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,18 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+  //res.send("you made it");
 });
 
 router.get('/stats', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/stats.html'));
+  res.sendFile(path.join(__dirname, '../Develop/public/stats.html'));
+  //res.send("you made it");
 });
 
 router.get('/exercise', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/exercise.html'));
+  res.sendFile(path.join(__dirname, '../Develop/public/exercise.html'));
+  //res.send("you made it");
 });
 
 module.exports = router;
