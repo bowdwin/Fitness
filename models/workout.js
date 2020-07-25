d6033f3bfbf1b485af2b335c33220a664ea2c7a2
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-07-25 15:58:10 -0400

    fixed model

--- models/workout.js
+++ models/workout.js
@@ -1,27 +1,36 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema([
-  {
-    workout: {
-      type: Date,
-      default: Date.now,
-    },
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
   },
-  {
-    exercises: [
-      {
-        exerciseType: String,
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true,
       },
-    ],
-  },
-]);
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: true,
+      },
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
+});
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,35 @@
 const router = require('express').Router();
 const db = require('../models/');
 
-router.put('/workouts/:id', (req, res) => {});
+router.put('/workouts/:id', (req, res) => {
+  //add
+});
 
-router.post('/workouts/', (req, res) => {});
+router.post('/workouts/', (req, res) => {
+  //add
+});
 
 router.get('/workouts/', (req, res) => {
   db.Workout.find({}, (err, workouts) => {
     if (err) {
       console.log(err);
     } else {
+      console.log(workouts);
       res.json(workouts);
     }
   });
+});
+
+router.get('/workouts/range', async (req, res) => {
+  try {
+    const data = await Workout.find({}).limit(7);
+
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 module.exports = router;
