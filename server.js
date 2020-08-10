cfffca10520792cc604aa472f9c504b2a9cae5af
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-08-10 16:33:14 -0400

    pushing readme

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3000;
-// const db = require('./models');
+
 const app = express();
-// middleware
-// app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
